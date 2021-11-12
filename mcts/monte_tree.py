1678b81c894ae485b76883f0ab5b0c4a76756d76
dowdyboy <1203550038@qq.com>
2021-11-12 22:19:29 +0800

    提交自我对弈代码

--- mcts/monte_tree.py
+++ mcts/monte_tree.py
@@ -13,6 +13,7 @@ class MonteChessTreeConfig:
         self.chess_size = (16, 16)
         self.first_player = MonteChessTreeConfig.PLAYER_BLACK
         self.c_puct = 1.
+        self.t = 2.
 
 
 class MonteChessState:
@@ -233,49 +234,102 @@ class MonteChessTreeNode:
     def is_over(self):
         return self.node_state == MonteChessTreeNode.STATE_BLACK_WIN or self.node_state == MonteChessTreeNode.STATE_WHITE_WIN or self.node_state == MonteChessTreeNode.STATE_TWO_WIN
 
+    def get_real_step(self):
+        prob = (self.N ** (1. / self.tree.conf.t)) / np.sum(self.N ** (1. / self.tree.conf.t))
+        prob[self.state.get_empty_position_mask() == False] = 0.
+        next_step = np.argmax(prob)
+        return next_step, (next_step // self.tree.conf.chess_size[1], next_step % self.tree.conf.chess_size[1])
+
 
 def test(model):
     conf = MonteChessTreeConfig()
     tree = MonteChessTree(conf, model)
+
     tree.reset()
 
-    cur_node = tree.root
+    while not tree.root.is_over():
+        print(tree.root.state.chess_state)
+        cur_node = tree.root
+        backward_count = 0
+        while backward_count < 5000:
+            is_extend_node = False
+            next_idx, next_pos = cur_node.select_next_position_idx()
+            if cur_node.has_child_node(next_idx):
+                cur_node.record_next_step(next_idx)
+                cur_node = cur_node.children[next_idx]
+            else:
+                is_extend_node = True
+                next_state = cur_node.state.copy()
+                next_state.update_chess_state(next_pos)
+                next_state.switch_player()
+                new_node = MonteChessTreeNode(tree)
+                new_node.init_state(next_state)
+                new_node.set_parent(cur_node)
+                cur_node.children[next_idx] = new_node
+                cur_node.record_next_step(next_idx)
+                cur_node = new_node
+            # 判断是否执行回传
+            if is_extend_node or cur_node.is_over():
 
-    for _ in range(10000):
-        is_extend_node = False
-        next_idx, next_pos = cur_node.select_next_position_idx()
-        if cur_node.has_child_node(next_idx):
-            cur_node.record_next_step(next_idx)
-            cur_node = cur_node.children[next_idx]
-        else:
-            is_extend_node = True
-            next_state = cur_node.state.copy()
-            next_state.update_chess_state(next_pos)
-            next_state.switch_player()
-            new_node = MonteChessTreeNode(tree)
-            new_node.init_state(next_state)
-            new_node.set_parent(cur_node)
-            cur_node.children[next_idx] = new_node
-            cur_node.record_next_step(next_idx)
-            cur_node = new_node
-        # 判断是否执行回传
-        if is_extend_node or cur_node.is_over():
-            if cur_node.is_over():
-                print('over')
-            p_node = cur_node.parent
-            while p_node is not None:
-                chess_pos, _ = p_node.get_next_step_and_clear()
                 v = 0.
-                if cur_node.node_state == MonteChessTreeNode.STATE_DOING:
-                    v = cur_node.V
-                else:
+                if cur_node.is_over():
+                    print('chess over')
                     v = 1. if cur_node.node_state == MonteChessTreeNode.STATE_BLACK_WIN else -1. if cur_node.node_state == MonteChessTreeNode.STATE_WHITE_WIN else 0.
-                p_node.N[chess_pos] = p_node.N[chess_pos] + 1
-                p_node.W[chess_pos] = p_node.W[chess_pos] + v
-                p_node.Q = p_node.W / (p_node.N + 1e-9)
-                p_node = p_node.parent
-                cur_node = cur_node.parent
-            cur_node = tree.root
+                else:
+                    v = cur_node.V
 
-    print(cur_node.children)
+                p_node = cur_node.parent
+                while p_node is not None:
+                    chess_pos, _ = p_node.get_next_step_and_clear()
+                    p_node.N[chess_pos] = p_node.N[chess_pos] + 1
+                    p_node.W[chess_pos] = p_node.W[chess_pos] + v
+                    p_node.Q = p_node.W / (p_node.N + 1e-9)
+                    p_node = p_node.parent
+                    cur_node = cur_node.parent
+                cur_node = tree.root
+                backward_count += 1
+
+        next_idx, next_pos = tree.root.get_real_step()
+        next_state = tree.root.state.copy()
+        next_state.update_chess_state(next_pos)
+        next_state.switch_player()
+        tree.reset(next_state)
+
+    print(tree.root.state.chess_state)
+
+# for _ in range(10000):
+#     is_extend_node = False
+#     next_idx, next_pos = cur_node.select_next_position_idx()
+#     if cur_node.has_child_node(next_idx):
+#         cur_node.record_next_step(next_idx)
+#         cur_node = cur_node.children[next_idx]
+#     else:
+#         is_extend_node = True
+#         next_state = cur_node.state.copy()
+#         next_state.update_chess_state(next_pos)
+#         next_state.switch_player()
+#         new_node = MonteChessTreeNode(tree)
+#         new_node.init_state(next_state)
+#         new_node.set_parent(cur_node)
+#         cur_node.children[next_idx] = new_node
+#         cur_node.record_next_step(next_idx)
+#         cur_node = new_node
+#     # 判断是否执行回传
+#     if is_extend_node or cur_node.is_over():
+#         if cur_node.is_over():
+#             print('over')
+#         p_node = cur_node.parent
+#         while p_node is not None:
+#             chess_pos, _ = p_node.get_next_step_and_clear()
+#             v = 0.
+#             if cur_node.node_state == MonteChessTreeNode.STATE_DOING:
+#                 v = cur_node.V
+#             else:
+#                 v = 1. if cur_node.node_state == MonteChessTreeNode.STATE_BLACK_WIN else -1. if cur_node.node_state == MonteChessTreeNode.STATE_WHITE_WIN else 0.
+#             p_node.N[chess_pos] = p_node.N[chess_pos] + 1
+#             p_node.W[chess_pos] = p_node.W[chess_pos] + v
+#             p_node.Q = p_node.W / (p_node.N + 1e-9)
+#             p_node = p_node.parent
+#             cur_node = cur_node.parent
+#         cur_node = tree.root
 
